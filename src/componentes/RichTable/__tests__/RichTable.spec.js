ab28d943df401d429359b4741e358cd3461f8206
jmiklevska <85491387+jmiklevska@users.noreply.github.com>
2022-11-25 18:22:17 +0200

    add test

--- src/componentes/RichTable/__tests__/RichTable.spec.js
+++ src/componentes/RichTable/__tests__/RichTable.spec.js
@@ -3,6 +3,9 @@ import {
     shallow,
 } from 'enzyme'
 
+import BaseRow from 'componentes/BaseRow'
+import InfiniteList from 'componentes/InfiniteList'
+
 import RichTable from '../RichTable'
 
 describe('RichTable', () => {
@@ -44,5 +47,105 @@ describe('RichTable', () => {
                 />,
             ),
         ).toMatchSnapshot()
+    })
+
+    test('onRow click', () => {
+        const spyOnRowClick = jest.fn()
+        const rerenderInfinitListMock = jest.fn()
+        const wrapper = shallow(
+            <RichTable
+                name="someName"
+                load={jest.fn()}
+                rerenderInfinitList={rerenderInfinitListMock}
+                changeSequence={jest.fn()}
+                removeSort={jest.fn()}
+                removeFilter={jest.fn()}
+                ControlPanel={jest.fn()}
+                visible={[
+                    'column1',
+                    'column2',
+                ]}
+                infinitListKey="infinitListKey"
+                setVisible={jest.fn()}
+                columns={[
+                    {
+                        id: 'column1',
+                        props: {
+                            column1: 'column1',
+                        },
+                        width: '80%',
+                    },
+                    {
+                        id: 'column2',
+                        filterField: 'column2',
+                        props: {
+                            column2: 'column2',
+                        },
+                    },
+
+                ]}
+                setMatchedResults={jest.fn()}
+                onRowClick={spyOnRowClick}
+                disabled={false}
+            />,
+        )
+
+        const baseRowWrapper = shallow(wrapper.find(InfiniteList).props().Row({
+            id: 1,
+        }))
+
+        baseRowWrapper.find(BaseRow).props().rowClick({})
+        expect(spyOnRowClick).toHaveBeenCalledWith({
+            id: 1,
+        }, rerenderInfinitListMock)
+    })
+
+    test('no onRow click on disabled', () => {
+        const spyOnRowClick = jest.fn()
+        const rerenderInfinitListMock = jest.fn()
+        const wrapper = shallow(
+            <RichTable
+                name="someName"
+                load={jest.fn()}
+                rerenderInfinitList={rerenderInfinitListMock}
+                changeSequence={jest.fn()}
+                removeSort={jest.fn()}
+                removeFilter={jest.fn()}
+                ControlPanel={jest.fn()}
+                visible={[
+                    'column1',
+                    'column2',
+                ]}
+                infinitListKey="infinitListKey"
+                setVisible={jest.fn()}
+                columns={[
+                    {
+                        id: 'column1',
+                        props: {
+                            column1: 'column1',
+                        },
+                        width: '80%',
+                    },
+                    {
+                        id: 'column2',
+                        filterField: 'column2',
+                        props: {
+                            column2: 'column2',
+                        },
+                    },
+
+                ]}
+                setMatchedResults={jest.fn()}
+                onRowClick={spyOnRowClick}
+                disabled
+            />,
+        )
+
+        const baseRowWrapper = shallow(wrapper.find(InfiniteList).props().Row({
+            id: 1,
+        }))
+
+        baseRowWrapper.find(BaseRow).props().rowClick({})
+        expect(spyOnRowClick).not.toBeCalled()
     })
 })
